82de45ee6525b3e576d17aaaab57637ee6721b5e
renancvitor <renan.vitor.cm@gmail.com>
2025-04-29 14:03:40 -0300

    Início do primeiro desafio do tema 02

---
+++ src/desafio01Tema02/ProgramaDesafio01Tema02.java
@@ -0,0 +1,22 @@
+package desafio01Tema02;
+
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class ProgramaDesafio01Tema02 {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        while(scan.hasNext()) {
+            int valor = scan.nextInt();
+
+            if (valor == -1) {
+                break;
+            }
+        }
+    }
+}
