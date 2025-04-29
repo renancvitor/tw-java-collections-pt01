0d3477524e9ba2849a1c8f251ae3122a6fe40c81
renancvitor <renan.vitor.cm@gmail.com>
2025-04-29 13:50:24 -0300

    Implementação Pessoa - início do estudo em List

--- src/javaCollection/Main.java
+++ src/javaCollection/Main.java
@@ -1,8 +1,19 @@
 package javaCollection;
 
+import modelCollection.Pessoa;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
 public class Main {
 
     public static void main(String[] args) {
         System.out.println("Hellot world!");
+
+        List<Pessoa> pessoas = new ArrayList<Pessoa>();
+        pessoas.add(new Pessoa(1, "TreinaWeb 01"));
+        pessoas.add(new Pessoa(2, "TreinaWeb 02"));
+        System.out.println(pessoas.toString());
     }
 }
