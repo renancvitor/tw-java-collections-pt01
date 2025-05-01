16ec1c06cb434d67d0d69b05f44b0863550aa608
renancvitor <renan.vitor.cm@gmail.com>
2025-05-01 08:57:18 -0300

    Resolução desafio 05 ChatGPT

---
+++ src/desafioChatGPT/SistemaTurmaAlunos.java
@@ -0,0 +1,47 @@
+package desafioChatGPT;
+
+import java.util.*;
+
+public class SistemaTurmaAlunos {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        Map<String, List<String>> cadastroTurma = new HashMap<>();
+
+        System.out.print("\nQuantas turmas deseja cadastrar?  ");
+        int n = scanner.nextInt();
+        scanner.nextLine();
+
+        System.out.println("\nDigite o nome da turma (sem espaços) e então dê um espaço para digitar " +
+                "o(s) nome(s) do(s) aluno(s).");
+        System.out.println("Exemplo: \nTurmaA Ana João Pedro");
+        System.out.println("Sua vez:");
+
+        for (int i = 0; i < n; i++) {
+            String linhaTurma = scanner.nextLine();
+            String[] alunos = linhaTurma.split(" ");
+
+            String turma = alunos[0];
+            for (int j = 1; j < alunos.length; j++) {
+                String aluno = alunos[j];
+                cadastroTurma.computeIfAbsent(aluno, k -> new ArrayList<>()).add(turma);
+            }
+        }
+
+        System.out.print("\nDigite a quantidade de alunos que deseja consultar:  ");
+        int m = scanner.nextInt();
+        scanner.nextLine();
+
+        for (int i = 0; i < m; i++) {
+            String alunoConsulta = scanner.nextLine();
+
+            if (cadastroTurma.containsKey(alunoConsulta)) {
+                System.out.println(cadastroTurma.get(alunoConsulta));
+            } else {
+                System.out.println("ALUNO NÃO ENCONTRADO");
+            }
+        }
+
+        scanner.close();
+    }
+}
