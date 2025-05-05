15126ab696364e46518d7421486f007c23fc5a38
renancvitor <renan.vitor.cm@gmail.com>
2025-05-04 21:07:59 -0300

    Primeiros contatos com Iterator

--- src/javaCollection/Main.java
+++ src/javaCollection/Main.java
@@ -4,6 +4,7 @@ import modelCollection.Pessoa;
 
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.Iterator;
 import java.util.List;
 
 public class Main {
@@ -14,6 +15,16 @@ public class Main {
         List<Pessoa> pessoas = new ArrayList<Pessoa>();
         pessoas.add(new Pessoa(1, "TreinaWeb 01"));
         pessoas.add(new Pessoa(2, "TreinaWeb 02"));
-        System.out.println(pessoas.toString());
+
+//        for (int i = 0; i < pessoas.size(); i++) {
+//            Pessoa p = pessoas.get(i);
+//            System.out.println(p);
+//        }
+        Iterator<Pessoa> iteratorPessoa = pessoas.iterator();
+        while (iteratorPessoa.hasNext()) {
+            Pessoa p = iteratorPessoa.next();
+            System.out.println(p);
+        }
+        System.out.println("Fim!");
     }
 }
