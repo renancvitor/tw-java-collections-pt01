996ba89f1b8bf233dd674522dbcb173864bb0d8a
renancvitor <renan.vitor.cm@gmail.com>
2025-05-04 21:14:41 -0300

    Entendendo a diferaça do uso do for e do Iterator

--- src/javaCollection/Main.java
+++ src/javaCollection/Main.java
@@ -3,28 +3,30 @@ package javaCollection;
 import modelCollection.Pessoa;
 
 import java.util.ArrayList;
-import java.util.Collection;
 import java.util.Iterator;
 import java.util.List;
 
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hellot world!");
+        System.out.println("\nHellot world!");
 
         List<Pessoa> pessoas = new ArrayList<Pessoa>();
         pessoas.add(new Pessoa(1, "TreinaWeb 01"));
         pessoas.add(new Pessoa(2, "TreinaWeb 02"));
 
-//        for (int i = 0; i < pessoas.size(); i++) {
-//            Pessoa p = pessoas.get(i);
-//            System.out.println(p);
-//        }
+        System.out.println("\nCom laço for");
+        for (int i = 0; i < pessoas.size(); i++) {
+            Pessoa p = pessoas.get(i);
+            System.out.println(p);
+        }
+
+        System.out.println("\nCom Iterator");
         Iterator<Pessoa> iteratorPessoa = pessoas.iterator();
         while (iteratorPessoa.hasNext()) {
             Pessoa p = iteratorPessoa.next();
             System.out.println(p);
         }
-        System.out.println("Fim!");
+        System.out.println("\nFim!");
     }
 }
