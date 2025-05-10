e7e2f505b6e812c29524a213c34fa2053cde841d
renancvitor <renan.vitor.cm@gmail.com>
2025-05-10 00:04:21 -0300

    Entendendo LinkedHashSet

--- src/javaCollection/MainSet.java
+++ src/javaCollection/MainSet.java
@@ -3,6 +3,7 @@ package javaCollection;
 import modelCollection.Pessoa;
 
 import java.util.HashSet;
+import java.util.LinkedHashSet;
 import java.util.Set;
 
 public class MainSet {
@@ -16,10 +17,14 @@ public class MainSet {
         set.add(3); // Não pode conter elementos duplicados!
         System.out.println(set);
 
-        Set<Pessoa> setPessoa = new HashSet<Pessoa>();
+        Set<Pessoa> setPessoa = new LinkedHashSet<Pessoa>();
         setPessoa.add(new Pessoa(1, "TreinaWeb 01"));
         setPessoa.add(new Pessoa(2, "TreinaWeb 02"));
         setPessoa.add(new Pessoa(3, "TreinaWeb 03"));
         System.out.println(setPessoa);
+
+        for (Pessoa p : setPessoa) {
+            System.out.println(p);
+        }
     }
 }
