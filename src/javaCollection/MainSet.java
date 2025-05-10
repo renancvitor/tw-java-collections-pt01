d979cebb817bf8b3cd3a398fcae47b857ac1cbe7
renancvitor <renan.vitor.cm@gmail.com>
2025-05-10 08:37:00 -0300

    Conhecendo o TreeSet

--- src/javaCollection/MainSet.java
+++ src/javaCollection/MainSet.java
@@ -2,13 +2,12 @@ package javaCollection;
 
 import modelCollection.Pessoa;
 
-import java.util.HashSet;
-import java.util.LinkedHashSet;
-import java.util.Set;
+import java.util.*;
 
 public class MainSet {
 
     public static void main(String[] args) {
+        System.out.println("HashSet");
         Set<Integer> set = new HashSet<>();
         set.add(1);
         set.add(2);
@@ -17,13 +16,30 @@ public class MainSet {
         set.add(3); // Não pode conter elementos duplicados!
         System.out.println(set);
 
-        Set<Pessoa> setPessoa = new LinkedHashSet<Pessoa>();
-        setPessoa.add(new Pessoa(1, "TreinaWeb 01"));
-        setPessoa.add(new Pessoa(2, "TreinaWeb 02"));
-        setPessoa.add(new Pessoa(3, "TreinaWeb 03"));
-        System.out.println(setPessoa);
+        System.out.println("LinkedhSet");
+        Set<Pessoa> set2 = new LinkedHashSet<Pessoa>();
+        set2.add(new Pessoa(1, "TreinaWeb 01"));
+        set2.add(new Pessoa(2, "TreinaWeb 02"));
+        set2.add(new Pessoa(3, "TreinaWeb 03"));
+        System.out.println(set2);
 
-        for (Pessoa p : setPessoa) {
+        for (Pessoa p : set2) {
+            System.out.println(p);
+        }
+
+        System.out.println("TreeSet");
+        // Armazena os elementos de acordo com a especificação da ordenação
+        // De acordo com a lógica implementado no compareTo, neste caso específico
+        NavigableSet<Pessoa> set3 = new TreeSet<Pessoa>(); // Também é possível fazer através de
+        // expressões lambda, sem o compareTo implementado na classe modelo
+        set3.add(new Pessoa(1, "TreinaWeb 01"));
+        set3.add(new Pessoa(2, "TreinaWeb 02"));
+        set3.add(new Pessoa(3, "TreinaWeb 03"));
+        System.out.println(set3);
+        Pessoa teste = set3.lower(new Pessoa(2, "TreinaWeb 02"));
+        System.out.println(teste);
+
+        for (Pessoa p : set3) {
             System.out.println(p);
         }
     }
