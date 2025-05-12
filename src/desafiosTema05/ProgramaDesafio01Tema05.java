41c73b2e095a2508cbbf78de3eff64a677e3b29f
renancvitor <renan.vitor.cm@gmail.com>
2025-05-11 23:18:17 -0300

    Preparação desafio de código 01 tema 05

---
+++ src/desafiosTema05/ProgramaDesafio01Tema05.java
@@ -0,0 +1,21 @@
+package desafiosTema05;
+
+import java.util.Scanner;
+
+public class ProgramaDesafio01Tema05 {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int t = scan.nextInt();
+        String[] pair_left = new String[t];
+        String[] pair_right = new String[t];
+
+        for (int i = 0; i < t; i++) {
+            pair_left[i] = scan.next();
+            pair_right[i] = scan.next();
+        }
+
+        //Coloque seu código aqui
+
+    }
+}
