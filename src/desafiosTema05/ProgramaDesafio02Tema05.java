d61209f9f1dbfd5f27d8f6b09b62281a7afa6ecc
renancvitor <renan.vitor.cm@gmail.com>
2025-05-12 12:39:56 -0300

    Preparação desafio de código 02 tema 05

---
+++ src/desafiosTema05/ProgramaDesafio02Tema05.java
@@ -0,0 +1,20 @@
+package desafiosTema05;
+
+import java.util.*;
+
+public class ProgramaDesafio02Tema05 {
+
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        int t = Integer.valueOf(s.nextLine());
+
+        List<String> nomes = new ArrayList<String>();
+
+        for (int i = 0; i < t; i++) {
+            nomes.add(s.nextLine());
+        }
+
+        //Coloque seu código aqui
+
+    }
+}
