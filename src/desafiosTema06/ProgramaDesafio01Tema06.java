07f0f93d2400250615468f9dce5311e254886a2b
renancvitor <renan.vitor.cm@gmail.com>
2025-05-12 22:32:23 -0300

    Preparação desafio de código 02 tema 06

---
+++ src/desafiosTema06/ProgramaDesafio01Tema06.java
@@ -0,0 +1,20 @@
+package desafiosTema06;
+
+import java.util.*;
+import java.lang.reflect.*;
+
+public class ProgramaDesafio01Tema06
+{
+    public static void main(String []argh)
+    {
+
+        Scanner sc = new Scanner(System.in);
+
+        while (sc.hasNext()) {
+            String input = sc.next();
+
+            
+
+        }
+    }
+}
