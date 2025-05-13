d302a902e95bab81554b9f78f99afb691571e10e
renancvitor <renan.vitor.cm@gmail.com>
2025-05-13 19:11:49 -0300

    Conclusão desafio de código 02 tema 06

--- src/desafiosTema06/PrioridadesDesafio02Tema06.java
+++ src/desafiosTema06/PrioridadesDesafio02Tema06.java
@@ -1,23 +1,35 @@
 package desafiosTema06;
 
-import java.util.Comparator;
-import java.util.List;
-import java.util.ArrayList;
+import java.util.*;
 
 public class PrioridadesDesafio02Tema06 {
 
     List<EstudanteDesafio02Tema06> getEstudantes(List<String> eventos) {
-        List<EstudanteDesafio02Tema06> estudantes = new ArrayList<>();
-
-        for (String evento : eventos) {
-            if (evento.startsWith())
-        }
-
-        estudantes.sort(
+        Queue<EstudanteDesafio02Tema06> fila = new PriorityQueue<>(
                 Comparator.comparingDouble(EstudanteDesafio02Tema06::getNota).reversed()
                         .thenComparing(EstudanteDesafio02Tema06::getNome)
-                        .thenComparingInt(EstudanteDesafio02Tema06::getId));
+                        .thenComparingInt(EstudanteDesafio02Tema06::getId)
+        );
 
-        return estudantes;
+        for (String evento : eventos) {
+            String[] eventoDividido = evento.split(" ");
+
+            if (eventoDividido[0].equals("ADICIONAR")) {
+                String nome = eventoDividido[1];
+                double nota = Double.parseDouble(eventoDividido[2]);
+                int id = Integer.parseInt(eventoDividido[3]);
+
+                fila.add(new EstudanteDesafio02Tema06(id, nome, nota));
+            } else if (eventoDividido[0].equals("SERVIR")) {
+                fila.poll();
+            }
+        }
+
+        List<EstudanteDesafio02Tema06> resultado = new ArrayList<>();
+        while (!fila.isEmpty()) {
+            resultado.add(fila.poll());
+        }
+
+        return resultado;
     }
 }
